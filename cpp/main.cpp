065b908db044b40e09e0caad03e833138a2d81bf
Jimmy Ostler <jtostler1@gmail.com>
2023-03-08 10:45:35 -0800

    Some edits for linux

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
 
 int main() {
-    std::cout << "Assemble";
+    int i;
+    i = i + 7;
+    return i;
 }
--- cpp/mainL.s
+++ cpp/mainL.s
@@ -1,15 +1,9 @@
 	.file	"main.cpp"
 	.text
-	.local	_ZStL8__ioinit
-	.comm	_ZStL8__ioinit,1,1
-	.section	.rodata
-.LC0:
-	.string	"Assemble"
-	.text
 	.globl	main
 	.type	main, @function
 main:
-.LFB1731:
+.LFB0:
 	.cfi_startproc
 	endbr64
 	pushq	%rbp
@@ -17,76 +11,14 @@ main:
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
-	leaq	.LC0(%rip), %rax
-	movq	%rax, %rsi
-	leaq	_ZSt4cout(%rip), %rax
-	movq	%rax, %rdi
-	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
-	movl	$0, %eax
+	addl	$7, -4(%rbp)
+	movl	-4(%rbp), %eax
 	popq	%rbp
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
-.LFE1731:
+.LFE0:
 	.size	main, .-main
-	.type	_Z41__static_initialization_and_destruction_0ii, @function
-_Z41__static_initialization_and_destruction_0ii:
-.LFB2229:
-	.cfi_startproc
-	endbr64
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	subq	$16, %rsp
-	movl	%edi, -4(%rbp)
-	movl	%esi, -8(%rbp)
-	cmpl	$1, -4(%rbp)
-	jne	.L5
-	cmpl	$65535, -8(%rbp)
-	jne	.L5
-	leaq	_ZStL8__ioinit(%rip), %rax
-	movq	%rax, %rdi
-	call	_ZNSt8ios_base4InitC1Ev@PLT
-	leaq	__dso_handle(%rip), %rax
-	movq	%rax, %rdx
-	leaq	_ZStL8__ioinit(%rip), %rax
-	movq	%rax, %rsi
-	movq	_ZNSt8ios_base4InitD1Ev@GOTPCREL(%rip), %rax
-	movq	%rax, %rdi
-	call	__cxa_atexit@PLT
-.L5:
-	nop
-	leave
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE2229:
-	.size	_Z41__static_initialization_and_destruction_0ii, .-_Z41__static_initialization_and_destruction_0ii
-	.type	_GLOBAL__sub_I_main, @function
-_GLOBAL__sub_I_main:
-.LFB2230:
-	.cfi_startproc
-	endbr64
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	movl	$65535, %esi
-	movl	$1, %edi
-	call	_Z41__static_initialization_and_destruction_0ii
-	popq	%rbp
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE2230:
-	.size	_GLOBAL__sub_I_main, .-_GLOBAL__sub_I_main
-	.section	.init_array,"aw"
-	.align 8
-	.quad	_GLOBAL__sub_I_main
-	.hidden	__dso_handle
 	.ident	"GCC: (Ubuntu 11.3.0-1ubuntu1~22.04) 11.3.0"
 	.section	.note.GNU-stack,"",@progbits
 	.section	.note.gnu.property,"a"
